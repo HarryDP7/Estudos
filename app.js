5eb1055f9d75e7e08cc11b9c5bd7325fa7130f8e
HarryDP7 <henrique.garcia777@gmail.com>
2024-06-11 22:13:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 7;
+let numeroMaximo = 5000
+let numeroSecreto = parseInt (Math.random() * numeroMaximo + 1) ;
 console.log(numeroSecreto);
 let chute; 
 let tentativas = 1;
 // enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-    alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;    
     } else {
         if (chute > numeroSecreto) {
          alert (`O número secreto é menor que ${chute}`);
@@ -19,3 +20,12 @@ while (chute != numeroSecreto){
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tenatativas' : 'tentativa'
+alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//if (tentativas > 1) {
+//    alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
